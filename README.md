63f96110b039cdc7c617e1764c275b5047a1c144
Michael Nolan <manolan@uw.edu>
2021-08-09 22:53:37 -0700

    Init: timeseries-prediction python package

---
+++ README.md
@@ -0,0 +1,9 @@
+# tspred: Timeseries Prediction
+
+This is a package of python code for creating, training and testing machine learning models for timeseries reconstruction and prediction tasks. Specifically, this code uses pytorch and pytorch-lightning to implement recon./pred. models for neural field potential (e.g. ECoG, LFP) signals datasets.
+
+Currently implemented models included in this package:
+- LFADS
+- MVAR
+
+Not currently licensed as of version 0.0.1
---
+++ setup.py
@@ -0,0 +1,23 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_decription = fh.read()
+
+setuptools.setup(
+    name="timeseries-prediction",
+    version="0.0.1",
+    author="Michael Nolan",
+    author_email="manolan@uw.edu",
+    description="Timeseries data reconstruction and prediction models in pytorch.",
+    long_decription=long_decription,
+    long_decription_content_type="test/markdown",
+    url="https://github.com/m-nolan/timeseries_prediction",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(where="src"),
+    python_requires=">=3.6",
+)
